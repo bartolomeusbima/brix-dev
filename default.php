a61da3020a63b10bd2ac7dab1041850a1be65dc4
bima <bm.santoso123@gmail.com>
2024-11-12 17:43:32 +0700

    update

--- default.php
+++ default.php
@@ -1,20 +1,3 @@
-<?php 
-    include 'config.php';
-    
-    $sql = "SELECT * FROM `master_article_head`";
-    $result = $conn->query($sql);
-    
-    if ($result->num_rows > 0) {
-        while ($row = $result->fetch_assoc()) {
-            echo "Article ID: " . $row['msarth_id'] . "<br>";
-        }
-    } else {
-        echo "No records found.";
-    }
-    
-    $conn->close();
-?>
-
 <!DOCTYPE html>
 <html lang="id">
     <head>
@@ -24,27 +7,15 @@
         <link rel="stylesheet" href="style.css" />
     </head>
     <body>
-        <div>Website visit count:</div>
-        <div class="website-counter"></div>
-
-        <script>
-            // When the page is loaded, increment the visitor count
-            window.onload = function() {
-                // Use AJAX to call the PHP script to update the visitor count
-                fetch('update_visitor.php')
-                    .then(response => response.json())
-                    .then(data => {
-                        if (data.success) {
-                            // Optionally, you can update the visitor count on the page
-                            document.querySelector('.website-counter').innerText = data.newVisitorCount;
-                        } else {
-                            console.error("Failed to update visitor count.");
-                        }
-                    })
-                    .catch(error => {
-                        console.error('Error:', error);
-                    });
-            }
-        </script>
+        <div class="login-page">
+            <div class="form">
+                <form class="login-form">
+                    <input type="text" placeholder="username"/>
+                    <input type="password" placeholder="password"/>
+                    <button>login</button>
+                    <p class="message">Not registered? <a href="#">Create an account</a></p>
+                </form>
+            </div>
+        </div>
     </body>
 </html>
