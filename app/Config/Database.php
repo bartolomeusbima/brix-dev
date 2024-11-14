3de340e03e38f70c52ed5c5d371c9f6b9f0e4c96
bima <bm.santoso123@gmail.com>
2024-11-14 22:20:00 +0700

    ganti database configuration

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -27,9 +27,9 @@ class Database extends Config
     public array $default = [
         'DSN'          => '',
         'hostname'     => 'localhost',
-        'username'     => '',
-        'password'     => '',
-        'database'     => '',
+        'username'     => 'u412444635_brix',
+        'password'     => 'Br1xperformance',
+        'database'     => 'u412444635_brix',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
         'pConnect'     => false,
