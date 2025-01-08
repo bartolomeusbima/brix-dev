a8e9a2cbf8f36cc537f1a3c9dcb0dd00b7d7cbb6
bima <bm.santoso123@gmail.com>
2025-01-08 20:49:19 +0700

    tes

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = 'https://lightblue-albatross-676009.hostingersite.com/';
+    public string $baseURL = 'https://mediumspringgreen-viper-790408.hostingersite.com/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
