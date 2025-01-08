34f19526f0e366fd671ae15e22057263d734801b
bima <bm.santoso123@gmail.com>
2025-01-08 21:30:47 +0700

    tes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,4 +6,5 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
-$routes->post('subscribe', 'Home::subscribe'); // Handle form submission
+// $routes->post('subscribe', 'Home::subscribe'); // Handle form submission
+$routes->get('home/testDb', 'Home::testDb'); // Defines a route for the testDb method
