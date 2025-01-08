dcfb858ade37d07f78b5aecbada7106a4cf2cab3
bima <bm.santoso123@gmail.com>
2025-01-08 22:27:27 +0700

    tes

--- app/Views/home.php
+++ app/Views/home.php
@@ -475,11 +475,17 @@
                         <div class="column list-block__item">
                             <div class="subscribe-form s-footer__subscribe">
                                 
-                            <?php if (session()->getFlashdata('message')): ?>
-                                <div class="alert">
-                                    <?= session()->getFlashdata('message') ?>
-                                </div>
-                            <?php endif; ?>
+                                <?php if (session()->getFlashdata('success')): ?>
+                                    <div class="alert alert-success">
+                                        <?= session()->getFlashdata('success') ?>
+                                    </div>
+                                <?php endif; ?>
+
+                                <?php if (session()->getFlashdata('error')): ?>
+                                    <div class="alert alert-danger">
+                                        <?= session()->getFlashdata('error') ?>
+                                    </div>
+                                <?php endif; ?>
                             
                                 <form action="<?= base_url('home/subscribe') ?>" method="POST">
                                     <input type="email" name="email" class="u-fullwidth text-center" placeholder="Your Email" title="The domain portion of the email address is invalid (the portion after the @)." pattern="^([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22))*\x40([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d))*(\.\w{2,})+$" required>
