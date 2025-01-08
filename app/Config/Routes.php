2ed85286a562cbea6ee62a5c7eccf8c724f558db
bima <bm.santoso123@gmail.com>
2025-01-08 21:19:36 +0700

    tes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,4 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->post('home/subscribe', 'Home::subscribe'); // Handle form submission
