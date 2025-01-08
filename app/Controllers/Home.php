dd7f9039df431559853f2001dffd9360052b9352
bima <bm.santoso123@gmail.com>
2025-01-08 21:56:22 +0700

    tes

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -16,22 +16,28 @@ class Home extends BaseController
         // Get the email from the form
         $email = $this->request->getPost('email');
         
-        // Check if the email is valid
-        if ($email) {
+        // Check if the email is provided and is valid
+        if ($email && filter_var($email, FILTER_VALIDATE_EMAIL)) {
             // Load the SubscriberModel
             $subscriberModel = new SubscriberModel();
             
             // Prepare the data to be inserted
-            $data = ['hss_email' => $email];
+            $data = [
+                'hss_email' => $email,
+                'hss_create_date' => date('Y-m-d H:i:s')  // Optional: add the creation date
+            ];
             
             // Insert the email into the database
             if ($subscriberModel->insert($data)) {
                 return redirect()->to('/')->with('message', 'Thank you for subscribing!');
             } else {
+                // If insertion fails
                 return redirect()->to('/')->with('message', 'Subscription failed. Please try again.');
             }
         } else {
-            return redirect()->to('/')->with('message', 'Please provide a valid email.');
+            // If email is not valid
+            return redirect()->to('/')->with('message', 'Please provide a valid email address.');
         }
     }
+
 }
