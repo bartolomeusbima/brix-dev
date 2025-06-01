580b8b193d7d54b8e39a02474b6f3764e4487f4e
Bartolomeus Bima Santoso <bartolomeus@Bartolomeuss-MacBook-Air.local>
2025-06-01 23:35:33 +0700

    test

--- app/Views/layouts/main.php
+++ app/Views/layouts/main.php
@@ -200,6 +200,7 @@
             <?= $this->renderSection('content') ?>
 
             <footer class="s-footer">
+
                 <div class="s-footer__bottom_mid row-footer">
                     <div class="column lg-3 md-4 stack-on-1000 s-footer__bottom-left">
 
@@ -210,7 +211,7 @@
                                 <p class="whatsapp">0812-8474-6544</p>
                             </a>
                             <p class="phone">0812-8474-6544</p>
-                            <a href="mailto:brixperformance@gmail.com">
+                            <a href="mailto:brixperformance@gmail.com">    
                                 <p class="mail">brixperformance@gmail.com</p>
                             </a>
                         </div>
@@ -240,27 +241,21 @@
                             </ul>
                         </div>
                     </div>
-
                     <div class="column lg-7 md-6 stack-on-1000 s-footer__bottom-left">
                         <h4 class="footer-title">Stay Connected!</h4>
                         <p class="footer-contact">Get the latest updates through our newsletter.</p>
-
                         <div class="column footersletter">
                             <div class="subscribe-form s-footer__subscribe">
                                 <form id="mc-form" class="mc-form" style="max-width: 500px;">
-                                    <input type="email" name="EMAIL" id="mce-EMAIL" class="u-fullwidth text-center"
-                                        placeholder="Your Email"
-                                        title="The domain portion of the email address is invalid (the portion after the @)."
-                                        pattern="^([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22))*\x40([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d))*(\\.\w{2,})+$"
-                                        required>
+                                    <input type="email" name="EMAIL" id="mce-EMAIL" class="u-fullwidth text-center" placeholder="Your Email" title="The domain portion of the email address is invalid (the portion after the @)." pattern="^([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22))*\x40([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d))*(\.\w{2,})+$" required>
                                     <input type="submit" name="subscribe" value="Subscribe" class="btn btn--primary u-fullwidth">
                                 </form>
                             </div>
                         </div>
-
-                        <p class="footer-subtitle">Your personal information is protected. View our <a href="<?= site_url('privacy-policy') ?>">Privacy Policy</a>.</p>
+                        <p class="footer-subtitle">Your personal information is protected. View our Privacy Policy.</p>
                     </div>
                 </div>
+        
             </footer>
         
         </div>
