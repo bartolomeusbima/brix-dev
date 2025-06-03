4170de45b79d57da3e35ddf1648e06eaf53f26a6
Bartolomeus Bima Santoso <bartolomeus@Bartolomeuss-MacBook-Air.local>
2025-06-03 23:26:18 +0700

    test

--- index.php
+++ index.php
@@ -194,35 +194,6 @@
                 </div>
             </header>
 
-            <section class="hero">
-                <div class="hero-bg"></div>
-
-                <div class="hero-content">
-                    <div class="column lg-6 tab-4 mob-2">
-                        <h1 id="hero-title">
-                            BRIX&reg;&nbsp;PERFORMANCE
-                        </h1>
-                        <h2 id="hero-subtitle">
-                            is&nbsp;all&nbsp;about&nbsp;motorsports
-                        </h2>
-                        <div id="hero-trailer">
-                            <a href="assets/videos/trailer-competition-series.mp4" id="hero-trailer-btn">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: #fff;"><path d="M7 6v12l10-6z"></path></svg>
-                                COMPETITION SERIES TRAILER
-                            </a>
-                        </div>
-                    </div>
-                    <div class="column lg-6 tab-8 mob-10">
-                        <img id="hero-photo" src="assets/images/photos/index/intro-bg.jpg" alt="">
-                    </div>
-                </div>
-
-            </section>
-
-            <video loop muted autoplay="autoplay" class="responsive-video">
-                <source src="assets/videos/brix-motorsport.mp4" type="video/mp4">
-            </video>
-
             <footer class="s-footer">
                 <div class="s-footer__bottom_mid row-footer">
                     <div class="column lg-3 md-4 stack-on-1000 s-footer__bottom-left">
