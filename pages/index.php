99e0ed250f403433e0e77727b29819af7f368fbb
Bartolomeus Bima Santoso <bartolomeus@Bartolomeuss-MacBook-Air.local>
2025-06-05 20:56:02 +0700

    1

--- pages/index.php
+++ pages/index.php
@@ -1,14 +1,3 @@
-<?php
-$page = $_GET['page'] ?? 'home';
-$file = __DIR__ . "/pages/{$page}.php";
-
-if (file_exists($file)) {
-    include $file;
-} else {
-    http_response_code(404);
-    echo "Page not found.";
-}
-
 <!DOCTYPE html>
 <html lang="en" class="no-js">
     <head>
