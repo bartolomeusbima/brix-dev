11bc2f67cc545875ba10e58ed333ecde0b44b11a
Bartolomeus Bima Santoso <bartolomeus@Bartolomeuss-MacBook-Air.local>
2025-06-06 11:29:42 +0700

    1

--- articles/bmw-e36-with-brix.php
+++ articles/bmw-e36-with-brix.php
@@ -50,7 +50,7 @@
                             <p>STREET SERIES</p>
                         </div>
                         <h2 class="article-title">
-                            BMW E36 With BRIXX.
+                            BMW E36 With BRIX.
                         </h2>
                         <h5 class="article-subtitle">
                             Street-Ready Braking Upgrade for BMW Enthusiasts.
