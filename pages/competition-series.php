234fb95a793cdc9e1f6fc3484624cd12824b7556
Bartolomeus Bima Santoso <bartolomeus@Bartolomeuss-MacBook-Air.local>
2025-06-06 12:17:34 +0700

    1

--- pages/competition-series.php
+++ pages/competition-series.php
@@ -348,7 +348,12 @@
                 </div>
             </section>
 
-            <section class="recent-articles">
+            <?php
+                $filterCategory = 'competition-series';
+                include 'templates/recent-articles.php';
+            ?>
+
+            <!-- <section class="recent-articles">
                 <div class="recent-articles-head">
                     <div class="column">
                         <h1 class="recent-articles-title">
@@ -449,7 +454,7 @@
                         </div>
                     </div>
                 </div>
-            </section>
+            </section> -->
 
             <?php
                 $basePath = '';
