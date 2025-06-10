0e4f8049753e27b6e5bd0f1d010047465af1085e
BARTOLOMEUS BIMA SANTOSO <bartolomeus.b.santoso@sat.co.id>
2025-06-11 00:32:10 +0700

    test

--- controllers/distributor-controller.php
+++ controllers/distributor-controller.php
@@ -1,5 +1,21 @@
 <?php
-require_once __DIR__ . '/../config/db-config.php';
+$host = 'srv1031.hstgr.io';
+$db   = 'u412444635_brix';
+$user = 'u412444635_brix';
+$pass = 'Br1x-P3rf0rm4nc3';
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$options = array(
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+);
+
+try {
+    $pdo = new PDO($dsn, $user, $pass, $options);
+} catch (\PDOException $e) {
+    die("Database connection failed: " . $e->getMessage());
+}
 
 function getFormattedDistributors(PDO $pdo) {
     $stmt = $pdo->prepare("
